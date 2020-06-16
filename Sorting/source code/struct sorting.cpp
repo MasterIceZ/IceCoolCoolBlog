204e0c576d9630a088b64218606d1036cafc6e96
AiceAeng <64594596+MasterIceZ@users.noreply.github.com>
2020-06-16 19:05:53 +0700

    added descrpition

--- Sorting/source code/struct sorting.cpp
+++ Sorting/source code/struct sorting.cpp
@@ -1,14 +1,15 @@
+///Struct Sorting
 #include<bits/stdc++.h>
 using namespace std;
 
 struct A{
   int x,y;
   bool operator < (cosnt A&o) cosnt{
-    if(x!=o.x)  return x< o.x ;
-    return y> o.y ;
+    if(x!=o.x)  return x< o.x ; ///sort x min to max
+    return y> o.y ;             ///sort y max to min
   }
 };
-A a[10010];
+A a[10010];                     ///array of struct
 int main (){
   int n;
   scanf("%d},&n);
@@ -16,7 +17,7 @@ int main (){
   for(i=0;i<n;i++){
     scanf("%d %d",&a[i].x,&a[i].y);
   }
-  sort(a,a+n);
+  sort(a,a+n);                  ///sorting
   for(i=0;i<n;i++){
     printf("%d %d\n",a[i].x,a[i].y);
   }
