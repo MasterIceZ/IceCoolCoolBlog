4f9fb4a39771204e26e8892aa8add27b8bc75df8
AiceAeng <64594596+MasterIceZ@users.noreply.github.com>
2020-06-25 15:43:06 +0700

    Add files via upload

---
+++ Code C++/Marker.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+	int n,x,m;
+	double a=0;
+	scanf("%d %d",&n,&x);
+	while(n--){
+		scanf("%d",&m);
+		a += (double)m*(100+(double)x)/100;
+		a = ceil(a);
+		//cout << a << endl;
+	}
+	printf("%.0lf",a);
+	return 0;
+}
