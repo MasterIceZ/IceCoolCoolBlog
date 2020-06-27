93765d17fce813c8864e726004569f77e7f4b994
AiceAeng <64594596+MasterIceZ@users.noreply.github.com>
2020-06-27 21:07:40 +0700

    Update Ski.py

--- Code Python/Ski.py
+++ Code Python/Ski.py
@@ -3,6 +3,16 @@ m = int(input())
 q = int(input())
 for i in range(0,q,1) :
   now = n+1;
+  mark = 0
   for j in range(0,m,1) :
     ski = int(input())
-    
+    if ski == 1 :
+      now+=1
+    else :
+      now-=1
+    if now>=0 or now>=2*n+1 :
+      mark=1
+  if mark==1 :
+    print("0")
+  else :
+    print("1")
